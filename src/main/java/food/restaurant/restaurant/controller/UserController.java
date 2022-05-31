3ebe3c7a0c8e725a8626ba69200106584b4d6915
Iris <iris.vilaseca1998@gmail.com>
2022-05-31 11:02:53 +0200

    controllers updated

--- src/main/java/food/restaurant/restaurant/controller/UserController.java
+++ src/main/java/food/restaurant/restaurant/controller/UserController.java
@@ -1,10 +1,11 @@
 package food.restaurant.restaurant.controller;
 
+import food.restaurant.restaurant.model.User;
 import food.restaurant.restaurant.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/user")
@@ -16,10 +17,33 @@ public class UserController {
     public UserController(UserService userService){
         this.userService = userService;
     }
+
     @GetMapping(value = "/hello")
     public String hello(){
         return "Hello";
     }
 
+    @GetMapping("/edit/all")
+    public List<User> listUser(){
+        return userService.findAllUsers();
+    }
 
+    @GetMapping("/edit/{id}")
+    public User findUserById(@PathVariable("id") Long id){
+        return userService.findUserById(id);
+    }
+
+    @PutMapping("/edit/")
+    public User editUserById(@RequestBody User user){
+        return userService.updateUser(user);
+    }
+
+    @DeleteMapping("/edit/delete/{id}")
+    public void deleteUser(@PathVariable Long id){
+        userService.deleteUser(id);
+    }
+    @PostMapping("/add")
+    public User addUser(@RequestBody User user){
+        return userService.addUser(user);
+    }
 }
--- src/main/java/food/restaurant/restaurant/model/User.java
+++ src/main/java/food/restaurant/restaurant/model/User.java
@@ -7,7 +7,7 @@ import java.io.Serializable;
 public class User implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(nullable = false, updatable = false)
+   // @Column(nullable = false, updatable = false)
     private Long id;
     private String name;
     private String address;
