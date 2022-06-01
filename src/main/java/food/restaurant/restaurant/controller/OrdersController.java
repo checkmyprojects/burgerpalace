428d6cf7c6cadd12fdb4a0c1ee3abcb895c64f15
checkmyprojects <notnow.plz+github@gmail.com>
2022-06-01 12:43:26 +0200

    mapping & cors

--- src/main/java/food/restaurant/restaurant/controller/OrdersController.java
+++ src/main/java/food/restaurant/restaurant/controller/OrdersController.java
@@ -3,12 +3,16 @@ package food.restaurant.restaurant.controller;
 import food.restaurant.restaurant.model.Orders;
 import food.restaurant.restaurant.service.OrdersService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins="http://localhost:4200/")
+@RequestMapping("/orders")
 public class OrdersController {
 
     private final OrdersService ordersService;
